05faf75ac0ed15132f6da6bfa336aad32da9e6cc
krishpriyakp <102854932+krishpriyakp@users.noreply.github.com>
2024-09-03 10:46:57 +0530

    Add files via upload

---
+++ krishnapriya/basics.js
@@ -0,0 +1,19 @@
+console.log("temperature")
+
+temperature = 45;
+if (temperature < 20) {
+    
+    console.log("cold")
+    
+}
+
+
+
+    else if (temperature <= 20 && temperature >= 30)
+{
+    console.log("normal")
+}
+else {
+    console.log("hot")
+}
+    
